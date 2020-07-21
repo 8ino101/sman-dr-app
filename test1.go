4bf79573b384a01d1c2e2f00bf38001103b83096
8ino101 <54948326+8ino101@users.noreply.github.com>
2020-07-21 18:46:00 +0200

    Add files via upload

--- test1.go
+++ test1.go
@@ -2,9 +2,37 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
+
+type mymap map[bool]string
 
 func main(){
 
-	fmt.Println("test")
+	mappa := mymap{
+
+		true: "top",
+		false: "flop",
+	}
+
+	fmt.Println("mappa:", mappa)
+	i := 0
+	var myslice = [100]int{}
+
+	for i <=99 {
+		var y bool 
+		x := rand.Intn(100)
+		toss := rand.Intn(2)
+		if toss == 1 {
+			y = true
+		}	else {
+			y = false
+		}
+		fmt.Println("--->", mappa[y])
+		myslice[i] = x
+		i++
+	}
+
+	//fmt.Printf("%v, %T\n", x,x)
+	fmt.Printf("%v, %T, %v\n", myslice, myslice, len(myslice))
 }
