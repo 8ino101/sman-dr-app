b298dcafcc93b2f96d18c460efca2ff49a81a95a
Ottavio Gremigni <gremigni@gmail.com>
2020-07-21 18:22:52 +0200

    update

--- test1.go
+++ test1.go
@@ -5,23 +5,33 @@ import (
 	"math/rand"
 )
 
+type mymap map[bool]string
+
 func main(){
+
+	mappa := mymap{
+
+		true: "top",
+		false: "flop",
+	}
+
+	fmt.Println("mappa:", mappa)
 	i := 0
 	var myslice = [100]int{}
 
 	for i <=99 {
+		var y bool 
 		x := rand.Intn(100)
-		if x % 50 != 0 {
-			
-			i++
-			continue
+		toss := rand.Intn(2)
+		if toss == 1 {
+			y = true
+		}	else {
+			y = false
 		}
+		fmt.Println("--->", mappa[y])
 		myslice[i] = x
 		i++
 	}
-
-		
-
 
 	//fmt.Printf("%v, %T\n", x,x)
 	fmt.Printf("%v, %T, %v\n", myslice, myslice, len(myslice))
