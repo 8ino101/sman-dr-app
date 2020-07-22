94f706207dd2b9151156481322e8483dafe889c1
Ottavio Gremigni <gremigni@gmail.com>
2020-07-22 17:36:04 +0200

    step3

--- test1.go
+++ test1.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"math/rand"
-)
+	"log"
+	"os"
+	)
+
 
 type mymap map[bool]string
 
@@ -19,8 +22,27 @@ const (
 	b
 	c 
 )
-func main(){
 
+var (
+    WarningLogger *log.Logger
+    InfoLogger    *log.Logger
+    ErrorLogger   *log.Logger
+)
+
+func init() {
+    file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+    WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+    ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func main() {
+
+	InfoLogger.Println("main started")
 	fmt.Printf("%v, %T\n", a, a)
 	fmt.Printf("%v, %T\n", b, b)
 	fmt.Printf("%v, %T\n", c, c)
@@ -50,4 +72,6 @@ func main(){
 
 	//fmt.Printf("%v, %T\n", x,x)
 	fmt.Printf("%v, %T, %v\n", myslice, myslice, len(myslice))
+	//WarningLogger.Println("warning")
+	InfoLogger.Println("end of prog")
 }
